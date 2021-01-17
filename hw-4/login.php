f7f8d10d16b782fcfa2bdf0312b050da2dd3c54a
Dajana99 <66513187+Dajana99@users.noreply.github.com>
2021-01-17 22:31:18 +0100

    Forme za upravljanje nalogom
    Ubacene forme za registraciju i prijavljivanje na nalog.

--- hw-4/login.php
+++ hw-4/login.php
@@ -7,6 +7,25 @@
 </head>
 
 <body>
+    <div class="contaniner">
+        <h1 class="text-center">FilmoviPregled</h1>
+        <h4 class="text-center">-Prijavite se na Vas nalog-</h4>
+        <form action="index.php" method="POST" class="container">
+            <div class="form-group">
+                <label>Email addresa ili korisnicko ime</label>
+                <input type="text" class="form-control" name="user_data">
+            </div>
+            <div class="form-group">
+                <label >Lozinka</label>
+                <input type="password" class="form-control" name="password">
+            </div>
+            <h6 class="text-center mb-4">Nemate nalog? Registrujte se <strong><a
+                        href="registracija.php">ovde</a></strong>.</h6>
+            <input type="hidden" name='login' value=1>
+            <button type="submit" class="btn btn-primary w-100">Prijavi se</button>
+        </form>
+    </div>
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
         integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
