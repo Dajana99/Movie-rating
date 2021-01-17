4408d28e55a29ec958382867ba07553b3732f279
Dajana99 <66513187+Dajana99@users.noreply.github.com>
2021-01-17 19:20:30 +0100

    Registracija korisnika
    Omogucena registracija korisnika.

--- hw-4/index.php
+++ hw-4/index.php
@@ -1,4 +1,26 @@
 <?php 
+    $database = new mysqli('localhost','root','','filmovipregled');
+    session_start();
+    if(isset($_POST['login'])){
 
-    echo 'Hello world';
+    }
+    if(isset($_POST['register'])){
+        $fname = $_POST['first_name'];
+        $lname = $_POST['last_name'];
+        $uname = $_POST['username'];
+        $email = $_POST['email'];
+        $password = md5($_POST['password']);
+        var_dump($_POST);
+        $sql = "INSERT INTO 
+        korisnici(ime,prezime,email,korisnicko_ime,lozinka)
+        VALUES ('$fname','$lname','$uname','$email','$password')";
+        if($database->query($sql) === TRUE){
+            $_SESSION['username'] = $uname;
+            header('location: pocetna.php');
+        }
+        else{
+            echo 'Greska ' . $database->error;
+        }
+
+    }
 ?>
--- hw-4/registracija.php
+++ hw-4/registracija.php
@@ -7,6 +7,37 @@
 </head>
 
 <body>
+    <div class="contaniner">
+        <h1 class="text-center">FilmoviPregled</h1>
+        <h4 class="text-center">-Registrujte Vas nalog-</h4>
+        <form action="index.php" method="POST" class="container">
+            <div class="form-group">
+                <label>Ime</label>
+                <input type="text" class="form-control" name="first_name">
+            </div>
+            <div class="form-group">
+                <label>Prezime</label>
+                <input type="text" class="form-control" name="last_name">
+            </div>
+            <div class="form-group">
+                <label>Korisnicko ime</label>
+                <input type="text" class="form-control" name="username">
+            </div>
+
+            <div class="form-group">
+                <label>Email addresa</label>
+                <input type="email" class="form-control" name="email">
+            </div>
+            <div class="form-group">
+                <label>Lozinka</label>
+                <input type="password" class="form-control" name="password">
+            </div>
+            <h6 class="text-center mb-4">Vec imate nalog? Prijavite se <strong><a href="login.php">ovde</a></strong>.
+            </h6>
+            <input type="hidden" name='register' value=1>
+            <button type="submit" class="btn btn-primary w-100">Registruj se</button>
+        </form>
+    </div>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
         integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
